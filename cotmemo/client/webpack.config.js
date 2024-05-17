402c38494e906c6d2e79592e59a6e68bb5f9b0cd
ALMIKO JASPER L. CABALLES <89283730+Mikoto239@users.noreply.github.com>
2024-05-17 23:21:23 +0800

    Update webpack.config.js

--- cotmemo/client/webpack.config.js
+++ cotmemo/client/webpack.config.js
@@ -1,9 +1,26 @@
 const path = require('path');
 
 module.exports = {
-  // ... other webpack configurations
-  resolve: {
-    fallback: { "path": require.resolve("path-browserify") }
-  },
-  mode: 'development',  
+    entry: './src/pages/login.jsx',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react'],
+                    },
+                },
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
 };
