458902bd83a4aa8ed3f87ac4bd8a65605efe5b08
ALMIKO JASPER L. CABALLES <89283730+Mikoto239@users.noreply.github.com>
2024-05-17 23:42:44 +0800

    Update App.js

--- cotmemo/client/src/App.js
+++ cotmemo/client/src/App.js
@@ -1,80 +1,79 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import Login from './pages/login';
-import Signup from './pages/signup';
-import Userdashboard from './pages/user/Userdashboard';
-import PrivateRoute from './components/privateroute';
-import Admindashboard from '../src/pages/admin/admind';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import AdminCreateMemo from './pages/admin/adminCreateMemo';
-import PrivateRouteAdmin from './components/privateadmindashboard';
-import PrivateRouteSecretary from './components/privateroutesecretary';
-import PrivateRouteUser from './components/privaterouteuser';
-import PrivateRouteUnregistedr from './components/privaterouteunregis';
-import Secretarydashboard from './pages/secretary/secretaryDashboard';
-import Adminfacultymanager from './pages/admin/adminfacultymanager';
-import Unregisterdashboard from './pages/user/Unregistereduser';
-import InviteMember from './pages/admin/inviteMember';
-import Usermemo from './pages/user/Usermemo';
+
+// Import components for different roles
+import Login from './pages/login';
+import Signup from './pages/signup';
+import AdminDashboard from './pages/admin/AdminDashboard';
+import SecretaryDashboard from './pages/secretary/SecretaryDashboard';
+import UserDashboard from './pages/user/UserDashboard';
+import UnregisterDashboard from './pages/user/UnregisterDashboard';
+import UserMemo from './pages/user/UserMemo';
 import MemoDetails from './components/Memodetails';
-import AdminMemoManager from './pages/admin/adminMemomanager';
-import AdminMemoDetails from './pages/admin/adminMemodetails';
-import Secretarycreatememo from './pages/secretary/secretaryuploadmemo';
-import Secretarymemomanager from './pages/secretary/secretarymemomanager';
-import Secretarymemodetails from './pages/secretary/secretarymemodetails';
-import Secretaryfacultymanager from './pages/secretary/secretaryfacultymanger';
-import SecretaryRecieveMemoDetails from './pages/secretary/secretaryrecievememodetails';
-import AdminRecieveMemoDetails from './pages/admin/adminrecievememodetails';
-import Admincalendar from './pages/admin/admincalendar';
-import Secretarycalendar from './pages/secretary/secretarycalendar';
-import Usercalendar from './pages/user/usercalendar';
-import AdminReport from './pages/admin/adminReport';
+import AdminCreateMemo from './pages/admin/AdminCreateMemo';
+import InviteMember from './pages/admin/InviteMember';
+import AdminMemoManager from './pages/admin/AdminMemoManager';
+import AdminMemoDetails from './pages/admin/AdminMemoDetails';
+import AdminRecieveMemoDetails from './pages/admin/AdminRecieveMemoDetails';
+import Admincalendar from './pages/admin/Admincalendar';
+import AdminReport from './pages/admin/AdminReport';
 import AdminsentReport from './pages/admin/AdminsentReport';
-import SecretaryReport from './pages/secretary/secretaryreportlist';
-import SecretarysentReport from './pages/secretary/secretarysentreport';
-import UserReport from './pages/user/userreportlist';
+import Adminfacultymanager from './pages/admin/AdminFacultyManager';
+import Secretarycreatememo from './pages/secretary/SecretaryCreateMemo';
+import Secretarymemomanager from './pages/secretary/SecretaryMemoManager';
+import Secretarymemodetails from './pages/secretary/SecretaryMemoDetails';
+import SecretaryRecieveMemoDetails from './pages/secretary/SecretaryRecieveMemoDetails';
+import Secretarycalendar from './pages/secretary/SecretaryCalendar';
+import SecretaryReport from './pages/secretary/SecretaryReport';
+import SecretarysentReport from './pages/secretary/SecretarySentReport';
+import Secretaryfacultymanager from './pages/secretary/SecretaryFacultyManager';
+import Usercalendar from './pages/user/UserCalendar';
+import UserReport from './pages/user/UserReport';
+
+const NotFound = () => <h1>404 Not Found</h1>;
 
 const App = () => {
   return (
     <>
       <ToastContainer />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/unregisteruser/dashboard" component={UnregisterDashboard} />
+          <Route exact path="/user/dashboard" component={UserDashboard} />
+          <Route exact path="/user/memo" component={UserMemo} />
+          <Route exact path="/user/memo/:memoId" component={MemoDetails} />
+          <Route exact path="/user/calendar" component={Usercalendar} />
+          <Route exact path="/user/report_list" component={UserReport} />
 
-      <BrowserRouter>
-        <Route exact path="/" component={Login} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/signup" component={Signup} />
+          <Route exact path="/admin/dashboard" component={AdminDashboard} />
+          <Route exact path="/admin/inviteMember" component={InviteMember} />
+          <Route exact path="/admin/faculty_Manager" component={Adminfacultymanager} />
+          <Route exact path="/admin/memo_create" component={AdminCreateMemo} />
+          <Route exact path="/admin/memo_manager" component={AdminMemoManager} />
+          <Route exact path="/admin/memo_Icreate/:memoId" component={AdminMemoDetails} />
+          <Route exact path="/admin/recieve_memo/:memoId" component={AdminRecieveMemoDetails} />
+          <Route exact path="/admin/calendar" component={Admincalendar} />
+          <Route exact path="/admin/report_list" component={AdminReport} />
+          <Route exact path="/admin/sent_report/:memoId" component={AdminsentReport} />
 
-        <PrivateRouteUnregistedr exact path="/unregisteruser/dashboard" component={Unregisterdashboard} />
+          <Route exact path="/secretary/dashboard" component={SecretaryDashboard} />
+          <Route exact path="/secretary/faculty_manager" component={Secretaryfacultymanager} />
+          <Route exact path="/secretary/memo_create" component={Secretarycreatememo}/>
+          <Route exact path="/secretary/memo_manager" component={Secretarymemomanager} />
+          <Route exact path="/secretary/memo_Icreate/:memoId" component={Secretarymemodetails} />
+          <Route exact path="/secretary/recieve_memo/:memoId" component={SecretaryRecieveMemoDetails} />
+          <Route exact path="/secretary/calendar" component={Secretarycalendar} />
+          <Route exact path="/secretary/report_list" component={SecretaryReport} />
+          <Route exact path="/secretary/sent_report/:memoId" component={SecretarysentReport} />
 
-
-        <PrivateRouteUser exact path="/user/memo" component={Usermemo} />
-        <PrivateRouteUser exact path="/user/dashboard" component={Userdashboard} />
-        <PrivateRouteUser exact path="/user/memo/:memoId" component={MemoDetails} />
-        <PrivateRouteUser exact path="/user/calendar" component={Usercalendar} />
-        <PrivateRouteUser exact path="/user/report_list" component={UserReport} />
-
-        <PrivateRouteAdmin exact path="/admin/dashboard" component={Admindashboard} />
-        <PrivateRouteAdmin exact path="/admin/inviteMember" component={InviteMember} />
-        <PrivateRouteAdmin exact path="/admin/faculty_Manager" component={Adminfacultymanager} />
-        <PrivateRouteAdmin exact path="/admin/memo_create" component={AdminCreateMemo} />
-        <PrivateRouteAdmin exact path="/admin/memo_manager" component={AdminMemoManager} />
-        <PrivateRouteAdmin exact path="/admin/memo_Icreate/:memoId" component={AdminMemoDetails} />
-        <PrivateRouteAdmin exact path="/admin/recieve_memo/:memoId" component={AdminRecieveMemoDetails} />
-        <PrivateRouteAdmin exact path="/admin/calendar" component={Admincalendar} />
-        <PrivateRouteAdmin exact path="/admin/report_list" component={AdminReport} />
-        <PrivateRouteAdmin exact path="/admin/sent_report/:memoId" component={AdminsentReport} />
-
-        <PrivateRouteSecretary exact path="/secretary/dashboard" component={Secretarydashboard} />
-        <PrivateRouteSecretary exact path="/secretary/faculty_manager" component={Secretaryfacultymanager} />
-        <PrivateRouteSecretary exact path="/secretary/memo_create" component={Secretarycreatememo}/>
-        <PrivateRouteSecretary exact path="/secretary/memo_manager" component={Secretarymemomanager} />
-        <PrivateRouteSecretary exact path="/secretary/memo_Icreate/:memoId" component={Secretarymemodetails} />
-        <PrivateRouteSecretary exact path="/secretary/recieve_memo/:memoId" component={SecretaryRecieveMemoDetails} />
-        <PrivateRouteSecretary exact path="/secretary/calendar" component={Secretarycalendar} />
-        <PrivateRouteSecretary exact path="/secretary/report_list" component={SecretaryReport} />
-        <PrivateRouteSecretary exact path="/secretary/sent_report/:memoId" component={SecretarysentReport} />
-      </BrowserRouter>
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
     </>
   );
 };
